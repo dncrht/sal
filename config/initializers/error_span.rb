a3259b24cff4eae839ed3f8dc4cca5b345ff9dd1
Dani <daniel.cr.ht@gmail.com>
2014-11-15 10:32:09 +0000

    Meaningful icon for errors

--- config/initializers/error_span.rb
+++ config/initializers/error_span.rb
@@ -2,7 +2,7 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   message = Array(instance.error_message).join(', ')
 
   if html_tag =~ /class="checkbox"/ || ((html_tag !~ /type="checkbox"/) && (html_tag !~ /label/))
-    %(<div class="has-error">#{html_tag}<span class="help-block">⤷ #{message}</span></div>).html_safe
+    %(<div class="has-error">#{html_tag}<span class="help-block">✖ #{message}</span></div>).html_safe
   else
     %(<div class="has-error">#{html_tag}</div>).html_safe
   end
